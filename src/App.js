c8c7716f518cab6ebce070d71ad92c26ab6e74bd
Aris Ramadhan <arisram22@gmail.com>
2021-03-10 07:44:48 +0700

    add login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 // import logo from './logo.svg';
-import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import Login from './pages/Login';
+import { useState } from 'react';
+import { Button } from 'react-bootstrap';
 
 function App() {
+  const [modalLogin, setModalLogin] = useState(false);
+
   return (
     <div>
-      
+      <Button variant="primary" onClick={() => setModalLogin(true)}>
+        Login
+      </Button>
+
+      <Login
+        show={modalLogin}
+        onHide={() => setModalLogin(false)}
+      />
     </div>
   );
 }
---
+++ src/pages/Login.js
@@ -0,0 +1,44 @@
+import { Button, Form, Modal } from 'react-bootstrap';
+
+function Login(props) {
+    return (
+      <Modal
+        {...props}
+        size="sm"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        {/* <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Modal heading
+          </Modal.Title>
+        </Modal.Header> */}
+        <Modal.Body >
+
+            <div className="form-title mb-3">
+                <h4 className="text-yellow">Login</h4>
+            </div>
+            <div className="d-flex flex-column">
+                <Form>
+                <Form.Group controlId="formBasicEmail">
+                    <Form.Control type="email" className="form-control input-bg" placeholder="Email"/>
+                    
+                </Form.Group>
+
+                <Form.Group controlId="formBasicPassword">
+                    <Form.Control type="password" className="form-control input-bg" placeholder="Password" />
+                </Form.Group>
+                <Button variant="brown" className="btn btn-block btn-round">Login</Button>
+                </Form>
+                
+                <div className="text-center text-muted delimiter mt-2">Don't have an account ? klick<a href="#" class="text-brown"> here</a>.</div>
+            </div>
+             
+        </Modal.Body>
+        {/* <Modal.Footer>
+          <Button onClick={props.onHide}>Close</Button>
+        </Modal.Footer> */}
+      </Modal>
+    );
+  }
+export default Login;
